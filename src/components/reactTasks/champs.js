d2845418bc8024c37c9220995d80f6feac0ff053
MG <mattgordon@hotmail.co.uk>
2017-03-09 16:07:54 +0000

    statechamp

--- src/components/reactTasks/champs.js
+++ src/components/reactTasks/champs.js
@@ -4,26 +4,26 @@ import React from 'react'
 export default class Champ extends React.Component {
 constructor() {
 super()
-this.state = { detailsVisible: false };
-  this.handleMouseEnter = e => this.setState({ detailsVisible: true });
-  this.handleMouseLeave = e => this.setState({ detailsVisible: false });
+this.state = { viewable: false };
+  this.handleMouseEnter = e => this.setState({ viewable: true });
+  this.handleMouseLeave = e => this.setState({ viewable: false });
 }
-  render() {
-    return (
-      <div onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
-        <img src={this.props.champ.file} />
-        { this.state.detailsVisible && <ChampDetails champ={this.props.champ} /> }
-      </div>
-    )
-  }
+render() {
+return (
+<div onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
+<img src={this.props.champ.file} />
+{ this.state.viewable && <ChampDetails champ={this.props.champ} /> }
+</div>
+)
+}
 }
 
 const ChampDetails = ({champ}) => (
-  <div>
-    <p>Name: { champ.name } </p>
-    <p>Role: { champ.role } </p>
-    <p>Difficulty: { champ.diff } </p>
-    <p>Price: { champ.price } </p>
-  </div>
-    )
+<div>
+  <p>Name: { champ.name } </p>
+  <p>Role: { champ.role } </p>
+  <p>Difficulty: { champ.diff } </p>
+  <p>Price: { champ.price } </p>
+ </div>
+  )
  
